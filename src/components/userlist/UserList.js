895f2010322a19731ccb679cc90048dc061099a9
ntlong <ntlong@humaxdigital.com>
2018-03-15 22:38:06 +0700

    change state ok

--- src/components/userlist/UserList.js
+++ src/components/userlist/UserList.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
 
 class UserList extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.changeToChatBox = this.changeToChatBox.bind(this);
+        }
+
+        changeToChatBox () {
+            browserHistory.push('/chatbox');
+        }
   render() {
     console.log(this.props);
     return (
       <div>
         <h1>This page display user list</h1>
+        <button onClick={this.changeToChatBox}> click me to change state</button>
       </div>
     );
   }
