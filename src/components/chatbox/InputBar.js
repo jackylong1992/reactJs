962396187d6eecd4863a96232ef76286e1ffb4b1
ntlong <ntlong@humaxdigital.com>
2018-03-19 00:59:40 +0700

    styling scroll bar

--- src/components/chatbox/InputBar.js
+++ src/components/chatbox/InputBar.js
@@ -1,10 +1,15 @@
 import React, {PropTypes} from 'react';
 
 const InputBar = ({onClick}) => {
+    function checkInput (event) {
+        if (event.keyCode == 13) {
+            onClick();
+        }
+    }
     return (
         <div id="inputBar">
         <label for="inputlg"></label>
-        <input className="form-control input-lg" id="inputBox" type="text"/>
+        <input className="form-control input-lg" id="inputBox" type="text" onKeyDown={checkInput}/>
         <button className="btn-primary btn-lg" onClick={onClick}>Send</button>
         </div>
     );
